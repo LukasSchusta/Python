a62c2fd83956dd3b623b341cd89492d2fd500f74
Lucas Schuster <lukinhasschusta@gmail.com>
2022-05-15 16:39:07 -0300

    Update ex072.py

--- CursoExercicios/ex072.py
+++ CursoExercicios/ex072.py
@@ -1,9 +1,11 @@
 tupla = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
          'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-n1 = int(input('Digite um número entre 0 e 20: '))
-if n1 < 0:
-    print('Você digitou um número negativo!')
-elif n1 > 20:
-    print('Você digitou um número maior que 20!')
-else:
-    print(tupla[n1])
+while True:
+    n1 = int(input('Digite um número entre 0 e 20: '))
+    if n1 < 0:
+        print('Você digitou um número negativo!')
+    elif n1 > 20:
+        print('Você digitou um número maior que 20!')
+    elif n1 > 0 and n1 < 20:
+        print(f'Você digitou o número {tupla[n1]}')
+        break
